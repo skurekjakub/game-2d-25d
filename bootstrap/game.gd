28ef8d42cb7a670f91a091e8111abbafd92f7c02
skurekj <skurekjakub@gmail.com>
2026-05-16 22:43:55 +0200

    feat(bootstrap): add Game autoload with run state and XP/leveling

---
+++ bootstrap/game.gd
@@ -0,0 +1,38 @@
+extends Node
+
+var run_state: RunState
+
+
+func _ready() -> void:
+	run_state = RunState.new()
+
+
+func start_run() -> void:
+	run_state = RunState.new()
+	run_state.max_hp = 100.0
+	run_state.hp = run_state.max_hp
+	run_state.xp = 0
+	run_state.level = 1
+	run_state.time_elapsed = 0.0
+	EventBus.run_started.emit()
+
+
+func xp_needed(for_level: int) -> int:
+	return 5 + for_level * 3
+
+
+func add_xp(amount: int) -> void:
+	run_state.xp += amount
+	EventBus.xp_gained.emit(amount)
+	while run_state.xp >= xp_needed(run_state.level):
+		run_state.xp -= xp_needed(run_state.level)
+		run_state.level += 1
+		EventBus.level_up.emit(run_state.level)
+
+
+class RunState:
+	var hp: float
+	var max_hp: float
+	var xp: int
+	var level: int
+	var time_elapsed: float
---
+++ tests/test_game.gd
@@ -0,0 +1,47 @@
+extends GdUnitTestSuite
+
+
+func _make_game() -> Node:
+	# auto_free prevents Node-instance leak warnings; we never add to the tree.
+	return auto_free(load("res://bootstrap/game.gd").new())
+
+
+func test_start_run_initializes_state() -> void:
+	var game: Node = _make_game()
+	game.start_run()
+	assert_float(game.run_state.hp).is_equal(game.run_state.max_hp)
+	assert_int(game.run_state.xp).is_equal(0)
+	assert_int(game.run_state.level).is_equal(1)
+	assert_float(game.run_state.time_elapsed).is_equal(0.0)
+
+
+func test_add_xp_below_threshold_does_not_level_up() -> void:
+	var game: Node = _make_game()
+	game.start_run()
+	game.add_xp(3)
+	assert_int(game.run_state.xp).is_equal(3)
+	assert_int(game.run_state.level).is_equal(1)
+
+
+func test_add_xp_at_threshold_levels_up() -> void:
+	var game: Node = _make_game()
+	game.start_run()
+	# xp_needed(1) = 5 + 1*3 = 8
+	game.add_xp(8)
+	assert_int(game.run_state.level).is_equal(2)
+	assert_int(game.run_state.xp).is_equal(0)
+
+
+func test_add_xp_overflow_carries_to_next_level() -> void:
+	var game: Node = _make_game()
+	game.start_run()
+	game.add_xp(10)  # 8 to level up, 2 carry
+	assert_int(game.run_state.level).is_equal(2)
+	assert_int(game.run_state.xp).is_equal(2)
+
+
+func test_xp_needed_curve() -> void:
+	var game: Node = _make_game()
+	assert_int(game.xp_needed(1)).is_equal(8)
+	assert_int(game.xp_needed(2)).is_equal(11)
+	assert_int(game.xp_needed(10)).is_equal(35)
